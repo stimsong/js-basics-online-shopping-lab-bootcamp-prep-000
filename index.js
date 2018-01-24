fa3be32ebb43da513b92b11bd503269c8337e567
Greg Stimson <stimsong@yahoo.com>
2018-01-24 15:45:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -68,5 +68,5 @@ function placeOrder(cardNumber) {
   if(!cardNumber) {
     return console.log("Sorry, we don't have a credit card on file for you.");
   }
-  console.log(`Your total cost is $${total})
+  console.log(`Your total cost is $${total()}, which will be billed to the card ${cardNumber}`);
 }
